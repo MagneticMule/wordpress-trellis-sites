dd60c8837c7fba144ebc92fef2a5516fbc7a48b3
Thomas Sweeney <skywriter@gmail.com>
2021-11-11 15:30:16 +0000

    fix(composer) filled out composer.json for fitnessrepublic

--- fitnessrepublic.co.uk/config/environments/staging.php
+++ fitnessrepublic.co.uk/config/environments/staging.php
@@ -14,4 +14,4 @@ use Roots\WPConfig\Config;
  * Example: `Config::define('DISALLOW_FILE_MODS', false);`
  */
 
-Config::define('DISALLOW_INDEXING', true);
+Config::define('DISALLOW_INDEXING', false);
