2e4c951ce1e791b836c226e9921abfdc3dbe2fda
Thomas Sweeney <skywriter@gmail.com>
2021-11-11 13:55:22 +0000

    feat(fitnessreublic.co.uk)

--- theforgederby.co.uk/web/app/themes/auberge/footer.php
+++ theforgederby.co.uk/web/app/themes/auberge/footer.php
@@ -13,19 +13,19 @@
 
 
 
-do_action( 'tha_content_bottom' );
-do_action( 'tha_content_after' );
+	do_action( 'tha_content_bottom' );
+	do_action( 'tha_content_after' );
 
-if ( ! apply_filters( 'wmhook_disable_footer', false ) ) {
-	do_action( 'tha_footer_before' );
-	do_action( 'tha_footer_top' );
-	do_action( 'tha_footer_bottom' );
-	do_action( 'tha_footer_after' );
-}
+	if ( ! apply_filters( 'wmhook_disable_footer', false ) ) {
+		do_action( 'tha_footer_before' );
+		do_action( 'tha_footer_top' );
+		do_action( 'tha_footer_bottom' );
+		do_action( 'tha_footer_after' );
+	}
 
-do_action( 'tha_body_bottom' );
+	do_action( 'tha_body_bottom' );
 
-wp_footer();
+	wp_footer();
 
 ?>
 
